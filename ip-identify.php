7ca3a51eb05445be5e39717cd166d268b407dd38
unknown <e93.didban@yahoo.com>
2026-02-13 19:00:05 +0330

    change the code structure

---
+++ ip-identify.php
@@ -0,0 +1,12 @@
+<?php
+
+$user_ip = '5.220.4.76';
+function is_has_prefix($ip_address, $prefix)
+{
+
+    if (strpos($ip_address, $prefix . '.') === 0)
+        return "This ip address <strong><mark>($ip_address)</mark></strong> has prefix <strong><mark>$prefix</mark></strong>";
+    return "This ip address <strong><mark>($ip_address)</mark></strong> has not the prefix <strong><mark>$prefix</mark></strong>";
+}
+
+echo (is_has_prefix($user_ip, 15));
