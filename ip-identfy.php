9a6a67b0dcc1f37f25625b24b5c81dffecb4d493
unknown <e93.didban@yahoo.com>
2026-02-13 18:39:24 +0330

    edit returning message

--- ip-identfy.php
+++ ip-identfy.php
@@ -7,9 +7,9 @@ function is_iran_ip($ip_address)
     $iran_ip_prefixes = ['2', '5', '81', '95', '112', '185'];
     foreach ($iran_ip_prefixes as $ip) {
         if (strpos($ip_address, $ip . '.') === 0)
-            return true;
+            return "This ip address ($ip_address) is from IRAN";
     }
-    return false;
+    return "This ip address ($ip_address) is not from IRAN";
 }
 
-echo (is_iran_ip($user_ip) ? 'Yes' : 'No');
+echo (is_iran_ip($user_ip));
